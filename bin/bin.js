5ba968ebcbeffb05dec43fb8f89feb95cc834e2c
Achille <achille.urbain@gmail.com>
2016-09-30 16:46:55 +0200

    Allow usage as a required module

---
+++ bin/bin.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const graphqlToJson = require('../index.js');
+const args = process.argv.slice(2);
+graphqlToJson({input: args[0], output: args[1]});
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-#!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-const args = process.argv.slice(2);
 
-// INPUT
-if (args.length === 0){
-  throw new Error('You must provide an input path.');
-}
-
-const filepath = path.resolve(args[0]);
-const schema = require(filepath);
-
-// Ugly hack. Since graphql uses instanceof everywhere, the schema we require is not considered a valid graphql Schema because it was not created with the graphql module installed in this module We have to retrieve the "original" graphql module
-const loaded_module = module.children.find(mod => path.normalize(mod.id) === filepath);
-const graphql = loaded_module.children.find(mod => mod.exports.graphql).exports;
-
-// OUTPUT
-let output = 'schema.json';
-if (args.length >= 2){
-  output = args[1];
+module.exports = ({input, output = 'schema.json'}, cb = () => {}) => {
+  // INPUT
+  if (!input){
+    throw new Error('You must provide an input path.');
+  }
+  const filepath = path.resolve(input);
+  const schema = require(filepath);
+  // Ugly hack. Since graphql uses instanceof everywhere, the schema we require is not considered a valid graphql Schema because it was not created with the graphql module installed in this module We have to retrieve the "original" graphql module
+  const loaded_module = module.children.find(mod => path.normalize(mod.id) === filepath);
+  const graphql = loaded_module.children.find(mod => mod.exports.graphql).exports;
+  // OUTPUT
   if (path.extname(output) === ''){
     output = path.join(output, 'schema.json');
   }
-}
 
-// WRITE
-graphql.graphql(schema, graphql.introspectionQuery)
-  .then(result => JSON.stringify(result, null, 2))
-  .then(json => fs.writeFileSync(output, json));
+  // WRITE
+  return graphql
+    .graphql(schema, graphql.introspectionQuery)
+    .then(result => JSON.stringify(result, null, 2))
+    .then(json => new Promise((resolve, reject) => fs.writeFile(output, json, (err) => {
+      if (err){
+        return reject(err);
+      }
+      cb();
+      resolve();
+    })));
+};

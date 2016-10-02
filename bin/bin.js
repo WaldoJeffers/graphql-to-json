40f66e83d5f1e35ea08e298971059d0952e72e26
Achille <achille.urbain@gmail.com>
2016-10-02 14:48:41 +0200

    Use Chalk for logs

--- bin/bin.js
+++ bin/bin.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
+const chalk = require('chalk');
 const graphqlToJson = require('../index.js');
 const util = require('../util.js');
 const args = process.argv.slice(2);
 const output = (args[1]) ? util.normalizePath(args[1]) : 'schema.json';
 graphqlToJson({input: args[0], output})
-  .then(() => process.stdout.write(`'${output} successfully created'`))
-  .catch(err => process.stdout.write(err));
+  .then(() => chalk.green(`'${output}' successfully created`))
+  .catch(err => chalk.red(err))
+  .then(str => process.stdout.write(str));
